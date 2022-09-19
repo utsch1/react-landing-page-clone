19cbb5ea059c5503556b6f90f53d0db1c964c5c1
Ute <utegreiner93@gmail.com>
2022-09-19 15:14:57 +0200

    Scaffold project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">sa</header>
+    </div>
+  );
+}
+
+export default App;
